9e6682583400510ab3d311458beeac846dafcb2c
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2022-11-08 08:43:02 -0600

    Creación de la clase Principal.java vacía

---
+++ Principal.java
@@ -0,0 +1,16 @@
+//
+//
+//
+//  @ Project: Laboratorio #4 de POO
+//  @ File Name: Principal.java
+//  @ Date: 08/11/2022
+//  @ Author: Carlos Alberto Valladares Guerra & Julio Andrés García Salas Pérez
+//
+//
+//
+
+public class Principal{
+    public static void main(String[] args) {
+        
+    }
+}
