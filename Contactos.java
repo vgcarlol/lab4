5632c9e446dda3e7dba459e876ae361ef0c0cbdf
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2022-11-09 13:11:49 -0600

    Creación de atributos y métodos.

--- Contactos.java
+++ Contactos.java
@@ -1,3 +1,77 @@
 public class Contactos {
+    private String nombre;
+    private int telefono;
+
+
+    public Contactos() {
+    }
+
+    public Contactos(String nombre, int telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
     
+    /** 
+     * @return String
+     */
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    
+    /** 
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    /** 
+     * @return int
+     */
+    public int getTelefono() {
+        return this.telefono;
+    }
+
+    
+    /** 
+     * @param telefono
+     */
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    
+    /** 
+     * @param nombre
+     * @return Contactos
+     */
+    public Contactos nombre(String nombre) {
+        setNombre(nombre);
+        return this;
+    }
+
+    
+    /** 
+     * @param telefono
+     * @return Contactos
+     */
+    public Contactos telefono(int telefono) {
+        setTelefono(telefono);
+        return this;
+    }
+
+    
+    /** 
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return
+            "Nombre del contacto: " + getNombre() + "" +
+            "\nNúmero de teléfono: " + getTelefono() + "";
+    }
+
 }
